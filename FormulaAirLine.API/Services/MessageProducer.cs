83522acbc9918cb22ede2fe38496e70a10a02b94
MarioTiara <mario.tiarapratama@gmail.com>
2022-12-15 21:02:32 +0700

    Implement MessagaeProcedure with rabbitMQ client

--- FormulaAirLine.API/Services/MessageProducer.cs
+++ FormulaAirLine.API/Services/MessageProducer.cs
@@ -1,12 +1,33 @@
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
 
 namespace FormulaAirLine.API.Services
 {
-    public class MessageProducer
+    public class MessageProducer : IMessageProducer
     {
-        
+        public void SendingMessage<T>(T message)
+        {
+            var vactory= new ConnectionFactory(){
+                HostName="localhost",
+                UserName="user",
+                Password="mypass",
+                VirtualHost="/"
+            };
+
+            var conn=vactory.CreateConnection();
+            using var channel=conn.CreateModel();
+
+            channel.QueueDeclare("bookings", durable:true, exclusive:false);
+
+            var jsonstring= JsonSerializer.Serialize(message);
+            var body= Encoding.UTF8.GetBytes(jsonstring);
+
+            channel.BasicPublish("", "bookings", body:body);
+        }
     }
 }
